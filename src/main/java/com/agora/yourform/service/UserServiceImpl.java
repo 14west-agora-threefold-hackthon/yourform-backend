8cbf6f55164b0b2acb76744321cb76cb78e07ccf
spirozzi-agora <spirozzi.agora@gmail.com>
2020-03-04 17:43:03 -0500

    Add third user, fix existing user info

--- src/main/java/com/agora/yourform/service/UserServiceImpl.java
+++ src/main/java/com/agora/yourform/service/UserServiceImpl.java
@@ -12,25 +12,32 @@ public class UserServiceImpl implements UserService {
 		UserInfo userInfo = new UserInfo();
 
 		if (Integer.valueOf(1).equals(id)) {
-			// Short content preferred
-			userInfo.setDevice("desktop");
-			userInfo.setLocation("New York, USA");
-			userInfo.setShortContentPastViewTimeSeconds(2);
-			userInfo.setLongContentPastViewTimeSeconds(0);
-			userInfo.setShortAudioPastViewTimeSeconds(3);
-			userInfo.setLongAudioPastViewTimeSeconds(0);
-			userInfo.setShortVideoPastViewTimeSeconds(4);
-			userInfo.setLongVideoPastViewTimeSeconds(0);
-		} else if (Integer.valueOf(2).equals(id)) {
 			// Long content preferred
-			userInfo.setDevice("desktop");
-			userInfo.setLocation("New York, USA");
+			userInfo.setDevice("mobile");
+			userInfo.setLocation("New York City, NY");
 			userInfo.setShortContentPastViewTimeSeconds(0);
 			userInfo.setLongContentPastViewTimeSeconds(2);
 			userInfo.setShortAudioPastViewTimeSeconds(0);
 			userInfo.setLongAudioPastViewTimeSeconds(3);
 			userInfo.setShortVideoPastViewTimeSeconds(0);
 			userInfo.setLongVideoPastViewTimeSeconds(4);
+
+		} else if (Integer.valueOf(2).equals(id)) {
+			// Short content preferred
+			userInfo.setDevice("mobile");
+			userInfo.setLocation("Baltimore, MD");
+			userInfo.setShortContentPastViewTimeSeconds(2);
+			userInfo.setLongContentPastViewTimeSeconds(0);
+			userInfo.setShortAudioPastViewTimeSeconds(3);
+			userInfo.setLongAudioPastViewTimeSeconds(0);
+			userInfo.setShortVideoPastViewTimeSeconds(4);
+			userInfo.setLongVideoPastViewTimeSeconds(0);
+		} else if (Integer.valueOf(3).equals(id)) {
+			// Short video only
+			userInfo.setDevice("mobile");
+			userInfo.setLocation("Sacramento, CA");
+			userInfo.setShortVideoPastViewTimeSeconds(5);
+			userInfo.setLongVideoPastViewTimeSeconds(0);
 		}
 
 		return userInfo;
